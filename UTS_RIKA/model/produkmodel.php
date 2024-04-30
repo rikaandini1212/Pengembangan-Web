05107072f4067cf0594c4e5334b70525802af778
rikaandini1212 <145329976+rikaandini1212@users.noreply.github.com>
2024-04-30 17:01:02 +0700

    UTS

---
+++ UTS_RIKA/model/produkmodel.php
@@ -0,0 +1,13 @@
+<?php
+class ProdukModel {
+    public function getProduk() {
+       
+        return array(
+            array('id' => 1, 'nama_produk' => 'HP', 'harga' => 5000000),
+            array('id' => 2, 'nama_produk' => 'Gamis', 'harga' => 300000),
+            array('id' => 3, 'nama_produk' => 'Bolu', 'harga' => 7000),
+            array('id' => 4, 'nama_produk' => 'Es Teh', 'harga' => 3000)
+        );
+    }
+}
+?>
